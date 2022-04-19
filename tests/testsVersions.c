0ef87e8f29237808af7b3fae394eb1e4f612dc57
Oussama la216646 <la216646@student.helha.be>
2022-04-19 04:56:48 +0200

    Implementation du module connexion dans les tests
    Implementation du module de connexion dans les tests plus correction des differentes erreur presentes
    Suppression de l ancien testsVersions suite a un changement de nom

--- tests/testsVersions.c
+++ tests/testsVersions.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <mysql.h>
 #include "../unity/unity.h"
 #include "../includes/versions.h"
 #include "../includes/connexion.h"
@@ -6,9 +7,24 @@
 static char erreur[255];
 static char path[]="";
 MYSQL *connexion;
-connexion = connexion_bd("localhost", "root", "", "voiture", erreur);
 
-void setup(){
+
+
+
+void createTabletest_succes(){
+    connexion = connexion_bd("localhost", "root", "", "voiture", erreur);
+    TEST_ASSERT_EQUAL_UINT8(1, createTableVersion(connexion, erreur));
+    fermerConnexion(connexion);
+}
+void createTabletest_impossible(){
+    connexion = connexion_bd("localhost", "root", "", "voiture", erreur);
+    TEST_ASSERT_EQUAL_UINT8(0, createTableVersion(connexion, erreur));
+    TEST_ASSERT_EQUAL_STRING("ERREUR: Impossible de creer la table version\n", erreur);
+    fermerConnexion(connexion);
+}
+
+void mysqlInsertStructVersion_succes(){
+    connexion = connexion_bd("localhost", "root", "", "voiture", erreur);
     Version *myTestVersion = (Version *) malloc(470);
     myTestVersion->id = (char *) malloc(sizeof(char) * 20);
     myTestVersion->name = (char *) malloc(sizeof(char) * 150);
@@ -18,41 +34,44 @@ void setup(){
     strcpy(myTestVersion->name,"S PZEV 4dr Wagon (2.5L 5cyl 5M)");
     strcpy(myTestVersion->horsepower,"140");
     strcpy(myTestVersion->modelNiceName,"jetta-sportwagen");
-}
-
-void clean(){
+    TEST_ASSERT_EQUAL_UINT8(0, mysqlInsertStructVersion(connexion, myTestVersion, erreur));
     free(myTestVersion->modelNiceName);
     free(myTestVersion->name);
     free(myTestVersion->horsepower);
     free(myTestVersion->id);
     free(myTestVersion);
-}
-
-void createTabletest_succes(){
-    TEST_ASSERT_EQUAL_UINT8(1, createTableVersion(connexion, erreur));
-}
-void createTabletest_impossible(){
-    TEST_ASSERT_EQUAL_UINT8(0, createTableVersion(connexion, erreur));
-    TEST_ASSERT_EQUAL_STRING("ERREUR: Impossible de creer la table version\n", erreur);
-}
-
-void mysqlInsertStructVersion_succes(){
-    setup();
-    TEST_ASSERT_EQUAL_UINT8(0, mysqlInsertStructVersion(connexion, myTestVersion, erreur));
+    fermerConnexion(connexion);
     clean();
 }
 
 void mysqlInsertStructVersion_impossible(){
     setup();
+    connexion = connexion_bd("localhost", "root", "", "voiture", erreur);
+    Version *myTestVersion = (Version *) malloc(470);
+    myTestVersion->id = (char *) malloc(sizeof(char) * 20);
+    myTestVersion->name = (char *) malloc(sizeof(char) * 150);
+    myTestVersion->horsepower = (char *) malloc(sizeof(char) * 150);
+    myTestVersion->modelNiceName = (char *) malloc(sizeof(char) * 150);
+    strcpy(myTestVersion->id,"200485020");
+    strcpy(myTestVersion->name,"S PZEV 4dr Wagon (2.5L 5cyl 5M)");
+    strcpy(myTestVersion->horsepower,"140");
+    strcpy(myTestVersion->modelNiceName,"jetta-sportwagen");
     TEST_ASSERT_EQUAL_UINT8(1, mysqlInsertStructVersion(connexion, myTestVersion, erreur));
     TEST_ASSERT_EQUAL_STRING("ERREUR: Insertion impossible\n", erreur);
+    free(myTestVersion->modelNiceName);
+    free(myTestVersion->name);
+    free(myTestVersion->horsepower);
+    free(myTestVersion->id);
+    free(myTestVersion);
+    fermerConnexion(connexion);
     clean();
 }
 
 void ajoutVersion_succes(){
     //base de donnee existante
+    connexion = connexion_bd("localhost", "root", "", "voiture", erreur);
     TEST_ASSERT_EQUAL_UINT8(0, ajoutVersion(connexion, path, erreur));
-    mysql_query(connexion, "SELECT * FROM Marque"))
+    mysql_query(connexion, "SELECT * FROM Marque");
     MYSQL_RES *result = mysql_store_result(connexion);
     MYSQL_ROW row;
     row = mysql_fetch_row(result);
@@ -61,14 +80,17 @@ void ajoutVersion_succes(){
     TEST_ASSERT_EQUAL_STRING("acura", row[2]);
     TEST_ASSERT_EQUAL_STRING("150", row[3]);
     mysql_free_result(result);
+    fermerConnexion(connexion);
 }
 
 
 
 void ajoutVersion_fichierNonOuvert(){
     //base de donnee existante
+    connexion = connexion_bd("localhost", "root", "", "voiture", erreur);
     TEST_ASSERT_EQUAL_UINT8(1, ajoutVersion(connexion, "test.txt", erreur));
     TEST_ASSERT_EQUAL_STRING("ERREUR: Fichier non ouvert", erreur);
+    fermerConnexion(connexion);
 }
 
 
